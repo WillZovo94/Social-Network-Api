e0b5c7a959dd8c16e9f5070f03a9c4c67b0ac783
William <bzfishmaster94@gmail.com>
2024-09-04 21:54:42 -0400

    Added models

---
+++ models/Thoughts.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+
+    }
+});
+
+thoughtSchema.virtual('formattedCreatedAt')
+.get(function() {
+    return this.createdAt.toDateString();
+})
